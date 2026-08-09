92b975226761a1f99900fae0778d4196f578a8e4
Abitha-1234 <111401629+Abitha-1234@users.noreply.github.com>
2026-08-10 05:13:52 +0530

    Add files via upload

---
+++ week/NumberType.js
@@ -0,0 +1,13 @@
+function NumberType(){
+    let x=-4;
+    if(x>0){
+        console.log('given number is possitive');
+    
+    }else if(x<0){
+        console.log('given number is Negative');
+
+    }
+    else{
+        console.log('given number is neutral');
+    }
+}NumberType();
