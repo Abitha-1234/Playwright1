f1ef50c906b54cb74b2c861cf9271c55cc67e95a
Abitha-1234 <111401629+Abitha-1234@users.noreply.github.com>
2026-08-10 16:06:46 +0530

    Add files via upload

---
+++ week/PremitiveDataType.js
@@ -0,0 +1,13 @@
+
+let userName='Abitha';
+console.log(typeof userName);
+let Organization='Testleaf';
+console.log(typeof Organization);
+let contactNumber='1234567';
+console.log(typeof contactNumber);
+let knowsAutomation=true;
+console.log(typeof knowsAutomation);
+let usePlaywright;
+console.log(typeof usePlaywright);
+
+
